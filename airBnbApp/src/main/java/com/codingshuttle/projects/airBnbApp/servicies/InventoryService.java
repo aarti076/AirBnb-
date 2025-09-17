ddd5aabf9f42207b1d12a288be50ba8fadc8f5bb
aarti <aartikanojiya9131@gmail.com>
2025-09-17 19:25:13 +0530

    added inventory service

---
+++ airBnbApp/src/main/java/com/codingshuttle/projects/airBnbApp/servicies/InventoryService.java
@@ -0,0 +1,8 @@
+package com.codingshuttle.projects.airBnbApp.servicies;
+
+import com.codingshuttle.projects.airBnbApp.entities.Room;
+
+public interface InventoryService {
+
+    void initilizeRoomForAYear(Room room);
+}
